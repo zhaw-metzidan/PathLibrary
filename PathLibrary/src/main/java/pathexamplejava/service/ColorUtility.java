bbcf34b18721a3de7a76974e5958785d14fa8ce3
Daniel <zhaw-metzidan@students.zhaw.ch>
2019-05-06 18:52:10 +0200

    Final PathLibrary
    PathLibrary set to final state. Can be deplyoed to JCenter and used by backend application

--- PathLibrary/src/main/java/pathexamplejava/service/ColorUtility.java
+++
@@ -1,8 +0,0 @@
-package pathexamplejava.service;
-
-public final class ColorUtility {
-
-	public static final String selected = "pumpkin";
-	public static final String notSelected = "asbestos";
-
-}
--- PathLibrary/src/main/java/pathexamplejava/service/exception/ExceptionDatabaseService.java
+++
@@ -1,7 +0,0 @@
-package pathexamplejava.service.exception;
-
-import pathexamplejava.service.AbstractDatabaseService;
-
-public class ExceptionDatabaseService extends AbstractDatabaseService {
-
-}
